362bb2ecaf7a001cef3a73baf6071519ad9d7974
emon007iu <emon007iu@gmail.com>
2022-05-18 18:11:15 +0600

    add card file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <Card></Card>
     </div>
   );
 }
